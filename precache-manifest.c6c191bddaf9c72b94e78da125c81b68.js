36ad121d9baca3a91e271a78fc86d646cc64be15
christopherpoland <chris.poland94@gmail.com>
2019-09-22 07:45:58 -0400

    Updates

---
+++ precache-manifest.c6c191bddaf9c72b94e78da125c81b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9be560c5e415669b3d6d",
+    "url": "/React-Cribbage-Counter/static/js/runtime~main.9be560c5.js"
+  },
+  {
+    "revision": "0b5768eefd64e3e5097a",
+    "url": "/React-Cribbage-Counter/static/js/main.0b5768ee.chunk.js"
+  },
+  {
+    "revision": "9ef807963481eb41277b",
+    "url": "/React-Cribbage-Counter/static/js/1.9ef80796.chunk.js"
+  },
+  {
+    "revision": "0b5768eefd64e3e5097a",
+    "url": "/React-Cribbage-Counter/static/css/main.e0c5b468.chunk.css"
+  },
+  {
+    "revision": "d1343fa75c9999e3d34f7d89323420e9",
+    "url": "/React-Cribbage-Counter/index.html"
+  }
+];
